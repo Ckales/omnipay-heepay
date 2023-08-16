398b31d8486a3af268127adbdbd5a08d138cad6d
李静 <chinglinet@msn.com>
2023-08-16 09:48:36 +0800

    修改相关语法写法、添加注释

--- src/Message/HeepayPurchaseRequest.php
+++ src/Message/HeepayPurchaseRequest.php
@@ -107,7 +107,7 @@ class HeepayPurchaseRequest extends AbstractRequest
      * 设置订单时间
      * @param $value
      * @return mixed
-     * @author 李静
+     * @author ChingLi
      */
     public function setOrderTime($value){
         return $this->setParameter('orderTime',$value);
@@ -117,69 +117,197 @@ class HeepayPurchaseRequest extends AbstractRequest
      * 设置卡号
      * @param $value
      * @return mixed
-     * @author 李静
+     * @author ChingLi
      */
     public function setCardNum($value){
         return $this->setParameter('cardNum',$value);
     }
+
+    /**
+     * 设置卡密
+     * @param $value
+     * @return mixed
+     * @author ChingLi
+     */
     public function setCardPwd($value){
         return $this->setParameter('cardPwd',$value);
     }
+
+    /**
+     * 设置金额
+     * @param $value
+     * @return mixed
+     * @author ChingLi
+     */
     public function setAmount($value){
         return $this->setParameter('amount',$value);
     }
+
+    /**
+     * 设置卡类型
+     * @param $value
+     * @return mixed
+     * @author ChingLi
+     */
     public function setCardType($value){
         return $this->setParameter('cardType',$value);
     }
+
+    /**
+     * 设置key
+     * @param $value
+     * @return mixed
+     * @author ChingLi
+     */
     public function setKey($value){
         return $this->setParameter('key',$value);
     }
+
+    /**
+     * 设置key
+     * @param $value
+     * @return mixed
+     * @author ChingLi
+     */
     public function setDesKey($value){
         return $this->setParameter('deskey',$value);
     }
+
+    /**
+     * 设置Partner
+     * @param $value
+     * @return mixed
+     * @author ChingLi
+     */
     public function setPartner($value){
         return $this->setParameter('partner',$value);
     }
+
+    /**
+     * 设置回调url
+     * @param $value
+     * @return mixed
+     * @author ChingLi
+     */
     public function setNotifyurl($value){
         return $this->setParameter('notifyurl',$value);
     }
-    public function getOrderId(){
-        return $this->getParameter('orderId');
-    }
-    public function getOrderTime(){
-        return $this->getParameter('orderTime');
-    }
-    public function getCardNum(){
-        return $this->getParameter('cardNum');
-    }
-    public function getCardPwd(){
-        return $this->getParameter('cardPwd');
-    }
-    public function getCardType(){
-        return $this->getParameter('cardType');
-    }
-    public function getAmount(){
-        return $this->getParameter('amount');
-    }
-    public function getKey(){
-        return $this->getParameter('key');
-    }
-    public function getDesKey(){
-        return $this->getParameter('deskey');
-    }
-    public function getPartner(){
-        return $this->getParameter('partner');
-    }
-    public function getNotifyurl(){
-        return $this->getParameter('notifyurl');
-    }
-    public function getDesc(){
-        return $this->getParameter('desc');
-    }
+
+    /**
+     * 设置描述
+     * @param $value
+     * @return mixed
+     * @author ChingLi
+     */
     public function setDesc($value){
         return $this->setParameter('desc',$value);
     }
+
+    /**
+     * 获取订单号
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getOrderId(){
+        return $this->getParameter('orderId');
+    }
+
+    /**
+     * 获取订单时间
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getOrderTime(){
+        return $this->getParameter('orderTime');
+    }
+
+    /**
+     * 获取卡号
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getCardNum(){
+        return $this->getParameter('cardNum');
+    }
+
+    /**
+     * 获取卡密
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getCardPwd(){
+        return $this->getParameter('cardPwd');
+    }
+
+    /**
+     * 获取卡类型
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getCardType(){
+        return $this->getParameter('cardType');
+    }
+
+    /**
+     * 获取金额
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getAmount(){
+        return $this->getParameter('amount');
+    }
+
+    /**
+     * 获取key
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getKey(){
+        return $this->getParameter('key');
+    }
+
+    /**
+     * 获取DesKey
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getDesKey(){
+        return $this->getParameter('deskey');
+    }
+
+    /**
+     * 获取Partner
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getPartner(){
+        return $this->getParameter('partner');
+    }
+
+    /**
+     * 回去回调url
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getNotifyurl(){
+        return $this->getParameter('notifyurl');
+    }
+
+    /**
+     * 获取描述
+     * @return mixed
+     * @author ChingLi
+     */
+    public function getDesc(){
+        return $this->getParameter('desc');
+    }
+
+    /**
+     * 获取终端
+     * @return mixed|string|null
+     * @author ChingLi
+     */
     public function getEndPoint(){
-        return $this->endpoint ? $this->endpoint : null;
+        return $this->endpoint ?: null;
     }
 }
